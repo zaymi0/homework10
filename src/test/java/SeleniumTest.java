5da3e51f159c99e7d253cc520018b0f712ab9e31
Михаил <@zaymi0>
2024-04-12 02:01:21 +0300

    hw10

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -3,9 +3,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import java.util.List;
 
 public class SeleniumTest {
 
-
-
+    @Test
+    public void myFirstTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        for (WebElement link : links) {
+            System.out.println(link.getAttribute("href"));
+        }
+        driver.findElement(By.linkText(links.get(9).getText())).click();
+        driver.quit();
+    }
 }
