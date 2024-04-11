f27978b18d7417bb91a2ccd97d824f870fb83225
Михаил <@zaymi0>
2024-04-12 00:33:11 +0300

    Start

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+
+
+}
